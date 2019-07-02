e84055c4f599415677a9548b8c6dff8af82042d3
Alejandro Saenz <alejandrosaenz117@gmail.com>
2019-07-01 21:38:38 -0400

    init commit

---
+++ src/books/dto/create-book.dto.ts
@@ -0,0 +1,9 @@
+
+// ./src/books/dto/create-book.dto.ts
+
+export class CreateBookDTO {
+    readonly id: number;
+    readonly title: string;
+    readonly description: string;
+    readonly author: string;
+}
